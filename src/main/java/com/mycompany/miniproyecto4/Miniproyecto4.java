c3ac09922007c65cb3b18226c80d5e9904e6e8f6
Santiago González <santiago.galvez@correounivalle.edu.co>
2023-01-09 15:55:25 -0500

    inicio proyecto

---
+++ src/main/java/com/mycompany/miniproyecto4/Miniproyecto4.java
@@ -0,0 +1,21 @@
+/*
+ * MINIPROYECTO #3
+ *
+ * INTEGRANTES: 
+ * John Freddy Belalcázar
+ * Hernán David Cisneros
+ * Santiago González Gálvez
+ *
+ * Grupo 01 FPOE 
+ *
+ * PROFESOR: Luis Yohany Romo Portilla 
+ */
+
+package com.mycompany.miniproyecto4;
+
+public class Miniproyecto4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
